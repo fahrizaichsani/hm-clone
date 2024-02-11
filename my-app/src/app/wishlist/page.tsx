a960277eca00a01c07465b89c8136bc29653e9a9
fahrizaichsani <fahrizaichsanirafif@gmail.com>
2024-02-11 14:18:24 +0700

    feat: protect wishlist page

--- my-app/src/app/wishlist/page.tsx
+++ my-app/src/app/wishlist/page.tsx
@@ -1,34 +1,36 @@
-"use client"
+import PageProtect from "@/components/page-protect";
 import WishlistCard from "@/components/wishlistCard";
 import React from "react";
 
 export default function Wishlist() {
   return (
     <>
-      <div className="flex justify-center items-center">
-        <div className="max-w-[1160px] mx-[121px] mt-[50px]">
-          <div className="text-xl font-sans font-semibold flex justify-center items-center">
-            <span>FAVORIT</span>
-          </div>
-          <div className="text-sm font-sans flex justify-end items-end mt-[50px] mr-[10px]">
-            <span>2 Barang</span>
-          </div>
-          <div className="flex flex-wrap justify-center items-center gap-[8px] mb-[80px]">
-            <WishlistCard />
-            <WishlistCard />
-            <WishlistCard />
-            <WishlistCard />
-            <WishlistCard />
-            <WishlistCard />
-            <WishlistCard />
-            <WishlistCard />
-            <WishlistCard />
-            <WishlistCard />
-            <WishlistCard />
-            <WishlistCard />
+      <PageProtect>
+        <div className="flex justify-center items-center">
+          <div className="max-w-[1160px] mx-[121px] mt-[50px]">
+            <div className="text-xl font-sans font-semibold flex justify-center items-center">
+              <span>FAVORIT</span>
+            </div>
+            <div className="text-sm font-sans flex justify-end items-end mt-[50px] mr-[10px]">
+              <span>2 Barang</span>
+            </div>
+            <div className="flex flex-wrap justify-center items-center gap-[8px] mb-[80px]">
+              <WishlistCard />
+              <WishlistCard />
+              <WishlistCard />
+              <WishlistCard />
+              <WishlistCard />
+              <WishlistCard />
+              <WishlistCard />
+              <WishlistCard />
+              <WishlistCard />
+              <WishlistCard />
+              <WishlistCard />
+              <WishlistCard />
+            </div>
           </div>
         </div>
-      </div>
+      </PageProtect>
     </>
   );
 }
